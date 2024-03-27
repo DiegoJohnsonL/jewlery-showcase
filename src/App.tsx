786df2b740763a1c8d449b90620e993a5fb00007
DiegoJohnsonL <diegojohnson46@gmail.com>
2024-03-27 15:03:17 -0500

    wip colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Bloom, EffectComposer, N8AO, ToneMapping } from "@react-three/postproce
 import * as THREE from 'three'
 import Loader from "./components/loader";
 import useWebgi from "./components/useWebgi";
+import React from "react";
 
 function Ring({ frame, diamonds, env, ...props} : any) {
   const { nodes, materials } = useGLTF('/3-stone-transformed.glb') as any
@@ -21,8 +22,9 @@ function Ring({ frame, diamonds, env, ...props} : any) {
     </group>
   )
 }
-function FiberScene() {
-  const { shadow, frame, diamonds } = { shadow: '#000000', frame: '#fff0f0', diamonds: '#ffffff' }
+function FiberScene({ frame, diamondColor: diamonds } : any) {
+  const shadow = '#000000';
+
   const env = useEnvironment({ files: 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/peppermint_powerplant_2_1k.hdr' })
    return (
     <Canvas shadows dpr={[1, 1.5]} gl={{ antialias: false }} camera={{ position: [-5, 5, 14], fov: 20 }}>
@@ -51,60 +53,62 @@ function FiberScene() {
 
 function App() {
   const webdi = useWebgi()
+  const [diamondColor, setDiamondColor] = React.useState('#ffffff');
+  const [frameColor, setFrameColor] = React.useState('#fff0f0');
   return (
-    <div>
-      <div className="w-full h-full grid grid-cols-2">
+    <div className="w-full h-full flex flex-col gap-4">
+      <div className="w-full grid grid-cols-2 gap-8">
         <div className="flex flex-col">
           <p>React Three Fiber</p>
-          <div id="canvas-container" className="h-full">
-            <FiberScene />
+          <div id="canvas-container" className="h-screen max-h-[70vh]">
+            <FiberScene diamondColor={diamondColor} frame={frameColor} />
           </div>
         </div>
         <div className="flex flex-col">
           <p>Webgi</p>
-          <div className="h-full">
+          <div className="h-screen max-h-[70vh]">
             {webdi}
           </div>
         </div>
       </div>
-      <div className="footer--container">
-		<div className="footer--menu">
-			<ul>
-				<li><img className="config--gem" alt="Config gem" width="32" height="32" src="/images/diamond.svg"/></li>
-				<li><img className="config--material" alt="Config Material" width="32" height="32" src="/images/material.svg"/></li>
-				<li><img className="config--ring" alt="Config Material" width="32" height="32" src="/images/ring.svg"/></li>
-			</ul>
-		</div>
-		<div className="gem--menu">
-			<ul className="colors--list">
-				<li className="ruby active"><img alt="Fancy color" width="32" height="32" src="/images/ruby.svg"/></li>
-				<li className="faint"><img alt="Fancy color" width="32" height="32" src="/images/faint.svg"/></li>
-				<li className="fancy"><img alt="Fancy color" width="32" height="32" src="/images/fancy.svg"/></li>
-				<li className="aqua"><img alt="Fancy color" width="32" height="32" src="/images/aqua.svg"/></li>
-				<li className="swiss"><img alt="Fancy color" width="32" height="32" src="/images/swiss.svg"/></li>
-				<li className="yellow"><img alt="Fancy color" width="32" height="32" src="/images/yellow.svg"/></li>
-				<li className="orange"><img alt="Fancy color" width="32" height="32" src="/images/orange.svg"/></li>
-				<li className="green"><img alt="Fancy color" width="32" height="32" src="/images/green.svg"/></li>
-				<li className="emerald"><img alt="Fancy color" width="32" height="32" src="/images/emerald.svg"/></li>
-				<li className="rose"><img alt="Fancy color" width="32" height="32" src="/images/rose.svg"/></li>
-				<li className="violet"><img alt="Fancy color" width="32" height="32" src="/images/violet.svg"/></li>
-				<li className="close-gems"><svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM15 9l-6 6M9 9l6 6" stroke="#52322B" strokeLinecap="round" strokeLinejoin="round"/></svg></li>
-			</ul>
-		</div>
-		<div className="materials--menu">
-			<ul className="materials--list">
-				<li className="default active"><img alt="default color" width="60" src="/images/default.png"/></li>
-				<li className="silver-gold"><img alt="silver-gold" width="60" src="/images/silver_gold.png"/></li>
-				<li className="silver-silver"><img alt="Fancy color" width="60" src="/images/silver_silver.png"/></li>
-				<li className="gold-gold"><img alt="Fancy color" width="60" src="/images/gold_gold.png"/></li>
-				<li className="rose-silver"><img alt="Fancy color" width="60" src="/images/rose_silver.png"/></li>
-				<li className="gold-rose"><img alt="Fancy color" width="60" src="/images/gold_rose.png"/></li>
-				<li className="rose-rose"><img alt="Fancy color" width="60" src="/images/rose_rose.png"/></li>
-				<li className="close-materials"><svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM15 9l-6 6M9 9l6 6" stroke="#52322B" strokeLinecap="round" strokeLinejoin="round"/></svg></li>
-			</ul>
-		</div>
-	</div>
 
+      <div className="footer--container">
+        {/* <div className="footer--menu">
+          <ul>
+            <li><img className="config--gem" alt="Config gem" width="32" height="32" src="/images/diamond.svg"/></li>
+            <li><img className="config--material" alt="Config Material" width="32" height="32" src="/images/material.svg"/></li>
+            <li><img className="config--ring" alt="Config Material" width="32" height="32" src="/images/ring.svg"/></li>
+          </ul>
+        </div> */}
+        <div className="gem--menu">
+          <ul className="colors--list">
+            {/* <li className="ruby active"><img alt="Fancy color" width="32" height="32" src="/images/ruby.svg"/></li> */}
+            <li onClick={()=>setDiamondColor("#CFECEC")} className="faint"><img alt="Fancy color" width="32" height="32" src="/images/faint.svg"/></li>
+            <li onClick={()=>setDiamondColor("#a9cbe2")} className="fancy"><img alt="Fancy color" width="32" height="32" src="/images/fancy.svg"/></li>
+            <li onClick={()=>setDiamondColor("#62cffe")} className="aqua"><img alt="Fancy color" width="32" height="32" src="/images/aqua.svg"/></li>
+            <li onClick={()=>setDiamondColor("#76dce4")} className="swiss"><img alt="Fancy color" width="32" height="32" src="/images/swiss.svg"/></li>
+            <li onClick={()=>setDiamondColor("#efe75b")} className="yellow"><img alt="Fancy color" width="32" height="32" src="/images/yellow.svg"/></li>
+            <li onClick={()=>setDiamondColor("#eb8e17")} className="orange"><img alt="Fancy color" width="32" height="32" src="/images/orange.svg"/></li>
+            <li onClick={()=>setDiamondColor("#17ebb5")} className="green"><img alt="Fancy color" width="32" height="32" src="/images/green.svg"/></li>
+            <li onClick={()=>setDiamondColor("#5eca00")} className="emerald"><img alt="Fancy color" width="32" height="32" src="/images/emerald.svg"/></li>
+            <li onClick={()=>setDiamondColor("#fa37d7")} className="rose"><img alt="Fancy color" width="32" height="32" src="/images/rose.svg"/></li>
+            <li onClick={()=>setDiamondColor("#c200f2")} className="violet"><img alt="Fancy color" width="32" height="32" src="/images/violet.svg"/></li>
+            {/* <li onClick={()=>setDiamondColor("")} className="close-gems"><svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM15 9l-6 6M9 9l6 6" stroke="#52322B" strokeLinecap="round" strokeLinejoin="round"/></svg></li> */}
+          </ul>
+        </div>
+        <div className="materials--menu">
+          <ul className="materials--list">
+            <li onClick={() => setFrameColor("#ffffff")} className="default active"><img alt="default color" width="60" src="/images/default.png"/></li>
+            <li onClick={() => setFrameColor("#fea04d")} className="silver-gold"><img alt="silver-gold" width="60" src="/images/silver_gold.png"/></li>
+            <li onClick={() => setFrameColor("#ffffff")} className="silver-silver"><img alt="Fancy color" width="60" src="/images/silver_silver.png"/></li>
+            <li onClick={() => setFrameColor("#fea04d")} className="gold-gold"><img alt="Fancy color" width="60" src="/images/gold_gold.png"/></li>
+            {/* <li onClick={() => setFrameColor("#fa8787")} className="rose-silver"><img alt="Fancy color" width="60" src="/images/rose_silver.png"/></li>
+            <li onClick={() => setFrameColor("")} className="gold-rose"><img alt="Fancy color" width="60" src="/images/gold_rose.png"/></li> */}
+            <li onClick={() => setFrameColor("#fa8787")} className="rose-rose"><img alt="Fancy color" width="60" src="/images/rose_rose.png"/></li>
+            {/* <li className="close-materials"><svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM15 9l-6 6M9 9l6 6" stroke="#52322B" strokeLinecap="round" strokeLinejoin="round"/></svg></li> */}
+          </ul>
+        </div>
+          </div>
     </div>
   );
 }
